58dcdbea5df6605ff9faa58ab160fa56bd79d405
emyrue <94935633+emyrue@users.noreply.github.com>
2023-06-14 18:33:21 -0600

    Signup request working

--- src/pages/signup_page.js
+++ src/pages/signup_page.js
@@ -1,14 +1,44 @@
 import { useState } from 'react';
-import { TextField } from '@mui/material';
+import { TextField, Button } from '@mui/material';
+import axios from 'axios';
 
 export default function SignupPage() {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post('http://127.0.0.1:4000/api/users',
+      {
+        user: {
+          name,
+          email,
+          password,
+          password_confirmation: passwordConfirmation,
+        },
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      });
+    console.log(response);
+  };
 
   return (
     <section>
       <h1>Sign Up</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="name"
+          label="Name"
+          variant="outlined"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <TextField
           id="email"
           label="Email"
@@ -23,6 +53,19 @@ export default function SignupPage() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        <TextField
+          id="password-confirmation"
+          label="Password Confirmation"
+          variant="outlined"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+        />
+        <Button
+          type="submit"
+          variant="outlined"
+        >
+          Submit
+        </Button>
       </form>
     </section>
   );

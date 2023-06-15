1176e49ff0b522d74741e24b49bf80c8a3fdc14e
emyrue <94935633+emyrue@users.noreply.github.com>
2023-06-14 19:20:29 -0600

    Add password visibility toggle to login page

--- src/pages/login_page.js
+++ src/pages/login_page.js
@@ -1,11 +1,23 @@
 import { useState } from 'react';
-import { TextField, Button } from '@mui/material';
+import {
+  TextField, Button, InputAdornment, IconButton, OutlinedInput, InputLabel, FormControl,
+} from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 // import axios from 'axios';
 
 export default function LoginPage() {
-  // const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleClickShowPassword = () => {
+    setShow(!show);
+  };
+
+  const handleMouseDownPassword = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <section>
@@ -18,14 +30,29 @@ export default function LoginPage() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <TextField
-          id="password"
-          label="Password"
-          variant="outlined"
-          value={password}
-          // type={show ? 'text' : 'password'}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <FormControl variant="outlined">
+          <InputLabel>Password</InputLabel>
+          <OutlinedInput
+            id="password"
+            label="Password"
+            variant="outlined"
+            value={password}
+            type={show ? 'text' : 'password'}
+            endAdornment={(
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {show ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+          )}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </FormControl>
         <Button
           type="submit"
           variant="outlined"

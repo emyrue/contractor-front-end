e38afce9e56423f74d9d97ec7431fb08113c2d21
emyrue <94935633+emyrue@users.noreply.github.com>
2023-06-21 18:15:46 -0600

    Added logout button

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,7 +1,14 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { userLogout } from '../redux/user/UserReducer';
 
 export default function Nav() {
+  const dispatch = useDispatch();
   const userInfo = useSelector((state) => state.user);
+
+  const handleClick = () => {
+    dispatch(userLogout());
+  };
+
   return (
     <nav>
       <a href="/">Home</a>
@@ -9,6 +16,8 @@ export default function Nav() {
         && <a href="/signup">Sign up</a>}
       { !userInfo.user.name
         && <a href="/login">Log in</a>}
+      { userInfo.user.name
+        && <button onClick={handleClick} type="button">Log out</button>}
     </nav>
   );
 }

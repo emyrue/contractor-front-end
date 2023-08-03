7a10df64fba67284bf9fd47842cc2ea89db4e4f7
emyrue <94935633+emyrue@users.noreply.github.com>
2023-08-03 16:52:38 -0600

    Users page for admin users

--- src/pages/users_page.js
+++ src/pages/users_page.js
@@ -5,12 +5,53 @@ import { getAllUsers } from '../redux/user/UserReducer';
 export default function UsersPage() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user.user);
+  const allUsers = useSelector((state) => state.user.allUsers);
 
   useEffect(() => {
     dispatch(getAllUsers());
-  }, [user]);
+  }, [dispatch, user]);
 
   return (
-    <section />
+    <section>
+      <h1>All Users</h1>
+      <ul>
+        { allUsers.map((user) => (
+          <li key={`user-${user.id}`}>
+            <h2>{user.name}</h2>
+            <h3>{user.email}</h3>
+            { user.contractor.name
+                && (
+                  <ul>
+                    <li>
+                      Contractor Name:
+                      {' '}
+                      {user.contractor.name}
+                    </li>
+                    <li>
+                      Rate:
+                      {' '}
+                      $
+                      {user.contractor.rate}
+                      /hr
+                    </li>
+                    <li>
+                      Rating:
+                      {' '}
+                      {user.contractor.rating}
+                      {' '}
+                      stars
+                      (
+                      {' '}
+                      {user.contractor.number_of_reviews}
+                      {' '}
+                      reviews
+                      )
+                    </li>
+                  </ul>
+                )}
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }

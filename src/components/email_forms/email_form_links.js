93f0c1fc16a246a28eace07a1e5be7e0bc47221d
emyrue <94935633+emyrue@users.noreply.github.com>
2023-08-10 13:49:01 -0600

    Resend confirmation form set up, not yet functional since the api calls are not in place

--- src/components/email_forms/email_form_links.js
+++ src/components/email_forms/email_form_links.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import { Button } from '@mui/material';
+import ResendConfirmation from './resend_confirmation';
 
 export default function EmailFormLinks() {
+  const [resendClass, setResendClass] = useState('hide');
+
+  const handleResendHide = () => {
+    setResendClass('hide');
+  };
+
   return (
     <ul>
       <li>
-        <Button>Resend Confirmation Instructions</Button>
+        <Button
+          onClick={() => setResendClass('')}
+        >
+          Resend Confirmation Instructions
+        </Button>
       </li>
+      <ResendConfirmation className={resendClass} handleClose={handleResendHide} />
     </ul>
   );
 }
--- src/components/email_forms/resend_confirmation.js
+++ src/components/email_forms/resend_confirmation.js
@@ -1,5 +1,42 @@
-export default function ResendConfirmation() {
+import { useState } from 'react';
+import { TextField, Button } from '@mui/material';
+import { PropTypes } from 'prop-types';
+
+export default function ResendConfirmation(props) {
+  const { className, handleClose } = props;
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleClose();
+  };
+
   return (
-    <form />
+    <form
+      className={className}
+      onSubmit={handleSubmit}
+    >
+      <TextField
+        id="resend-email"
+        label="Email"
+        value={email}
+        onChange={setEmail}
+      />
+      <Button
+        type="submit"
+      >
+        Submit
+      </Button>
+      <Button
+        onClick={handleClose}
+      >
+        Cancel
+      </Button>
+    </form>
   );
 }
+
+ResendConfirmation.propTypes = {
+  className: PropTypes.string.isRequired,
+  handleClose: PropTypes.func.isRequired,
+};

7e3b818bb72dd08bda38dd9dd72ac63be7c40c40
emyrue <94935633+emyrue@users.noreply.github.com>
2023-08-14 13:56:03 -0600

    Submitted notification shows up

--- src/components/email_forms/resend_confirmation.js
+++ src/components/email_forms/resend_confirmation.js
@@ -7,12 +7,14 @@ import endpoint from '../../redux/endpoint';
 export default function ResendConfirmation(props) {
   const { handleClose } = props;
   const [email, setEmail] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     axios.post(`${endpoint}users/confirmation`, {
       user: { email },
     });
+    setSubmitted(true);
   };
 
   return (
@@ -33,6 +35,21 @@ export default function ResendConfirmation(props) {
       >
         Cancel
       </Button>
+      { submitted
+        && (
+        <div>
+          <p>
+            If there is an account associated with the submitted email address,
+            {' '}
+            you will receive an email with confirmation instructions within the next few minutes.
+          </p>
+          <Button
+            onClick={handleClose}
+          >
+            Ok
+          </Button>
+        </div>
+        )}
     </form>
   );
 }

9e92308e1a468251159a7ac74299981e66ec711a
emyrue <94935633+emyrue@users.noreply.github.com>
2023-09-18 17:13:19 -0600

    Styled are you sure delete contractor form

--- src/components/contractors/delete_user_contractor.js
+++ src/components/contractors/delete_user_contractor.js
@@ -5,33 +5,36 @@ export default function DeleteUserContractor(props) {
   const { setDeleteDisplay, handleDelete } = props;
 
   return (
-    <div>
-      <span>
-        Are you sure? This will delete all information
-        {' '}
-        regarding your status as a contractor,
-        {' '}
-        including reviews and reservations from clients.
-        {' '}
-        Reservations and reviews that you have made for other contractors
-        {' '}
-        will remain intact.
-      </span>
-      <form onSubmit={handleDelete}>
-        <Fab
-          variant="extended"
-          type="submit"
-        >
-          Yes, delete my information
-        </Fab>
-        <Fab
-          variant="extended"
-          onClick={() => setDeleteDisplay(false)}
-        >
-          No, do not delete my information
-        </Fab>
-      </form>
-    </div>
+    <article className="delete-contractor">
+      <div>
+        <h3>Are you sure?</h3>
+        <span>
+          This will delete all information
+          {' '}
+          regarding your status as a contractor,
+          {' '}
+          including reviews and reservations from clients.
+          {' '}
+          Reservations and reviews that you have made for other contractors
+          {' '}
+          will remain intact.
+        </span>
+        <form onSubmit={handleDelete}>
+          <Fab
+            variant="extended"
+            type="submit"
+          >
+            Delete
+          </Fab>
+          <Fab
+            variant="extended"
+            onClick={() => setDeleteDisplay(false)}
+          >
+            Don&apos;t delete
+          </Fab>
+        </form>
+      </div>
+    </article>
   );
 }
 

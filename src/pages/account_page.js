0d9328f6b6da72a46f3ad8a6d24ea881029d74de
emyrue <94935633+emyrue@users.noreply.github.com>
2023-09-18 14:23:39 -0600

    Align heading for account page

--- src/pages/account_page.js
+++ src/pages/account_page.js
@@ -1,12 +1,13 @@
 import { useSelector } from 'react-redux';
 import UserDetails from '../components/user/user_details';
 import ContractorDetails from '../components/contractors/contractor_details';
+import '../styles/accountPage.scss';
 
 export default function AccountPage() {
   const userInfo = useSelector((state) => state.user);
 
   return (
-    <section>
+    <section className="account-page-section">
       { userInfo.isLoading
         && <h1>Loading...</h1>}
       { (!userInfo.user.name && !userInfo.isLoading)
